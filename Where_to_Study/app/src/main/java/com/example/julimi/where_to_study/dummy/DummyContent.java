fcd17e3750fa822b4361cdd8229f231dd0cb7c81
Weifeng Jiang <w48jiang@uwaterloo.ca>
2017-02-22 00:01:48 -0500

    initial 2

--- Where_to_Study/app/src/main/java/com/example/julimi/where_to_study/dummy/DummyContent.java
+++ Where_to_Study/app/src/main/java/com/example/julimi/where_to_study/dummy/DummyContent.java
@@ -18,7 +18,12 @@ import java.util.List;
 import java.util.Map;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.io.File;
+import android.os.Environment;
+import 	java.io.FileOutputStream;
+import 	java.io.OutputStreamWriter;
 
+import android.content.Context;
 /**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
@@ -37,13 +42,60 @@ public class DummyContent {
      */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
+    /**
+     * write string to a file
+     * @param data: the string to output
+     * @param filename: file name
+     */
+    public static void writeToFile(String data,String filename)
+    {
+        // Get the directory for the user's public pictures directory.
+        final File path =
+                Environment.getExternalStoragePublicDirectory
+                        (
+                                //Environment.DIRECTORY_PICTURES
+                                Environment.DIRECTORY_DCIM + "/buildings/"
+                        );
+
+        // Make sure the path directory exists.
+        if(!path.exists())
+        {
+            // Make it, if it doesn't exit
+            path.mkdirs();
+        }
+
+        final File file = new File(path, filename);
+
+        // Save your stream, don't forget to flush() it before closing it.
+
+        try
+        {
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+            myOutWriter.append(data);
+
+            myOutWriter.close();
+
+            fOut.flush();
+            fOut.close();
+        }
+        catch (IOException e)
+        {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
     private static final int COUNT = 25;
-    private static final String GET_URL = "https://api.uwaterloo.ca/v2/courses/CS/446/schedule.json?key=2d5402f20d57e1dd104101f9fa7dae27";
+    private static final String GET_URL_PRE = "https://api.uwaterloo.ca/v2/buildings/MC/";
+    private static final String GET_URL_POST = "/courses.json?key=2d5402f20d57e1dd104101f9fa7dae27";
     private static final String USER_AGENT = "Marshmallow/6.0";
+    private static int Len = 0;
+
     public static JSONObject jsonObject = new JSONObject();
     public static StringBuilder responseStrBuilder;
 
-    public static void sendGet() throws IOException {
+    public static void sendGet(String GET_URL) throws IOException {
         URL obj = new URL(GET_URL);
 
         // obtain a new connection
@@ -53,7 +105,7 @@ public class DummyContent {
         urlConnection.setRequestMethod("GET");
         urlConnection.setRequestProperty("User_Agent", USER_AGENT);
         int response = urlConnection.getResponseCode();
-        System.out.println("Connection Response Code: " + response);
+        //System.out.println("Connection Response Code: " + response);
 
         // read the request
         try {
@@ -66,7 +118,8 @@ public class DummyContent {
             while ((inStr = streamReader.readLine()) != null) responseStrBuilder.append(inStr);
 
             jsonObject = new JSONObject(responseStrBuilder.toString());
-            Log.d("","JSON value: " + jsonObject.getJSONArray("data").getJSONObject(0).getString("subject"));
+           // Log.d("","JSON value: " + jsonObject.getJSONArray("data").length());
+            Len = jsonObject.getJSONArray("data").length();
             in.close();
 
         } catch (JSONException e) {
@@ -82,7 +135,19 @@ public class DummyContent {
         @Override
         protected String doInBackground(String... params) {
             try {
-                DummyContent.sendGet();
+                String GET_URL;
+                for (int i = 1000; i < 1100; i++) {
+                    GET_URL = GET_URL_PRE + Integer.toString(i) + GET_URL_POST;
+
+
+                    DummyContent.sendGet(GET_URL);
+                    if (Len != 0) {
+                        System.out.println(i);
+                        writeToFile("{\"data\":[{","MC.txt");
+                        writeToFile("\"subject\":","MC.txt");
+                        //write
+                    }
+                }
                 System.out.println("1111111111111");
             } catch (IOException e) {
                 e.printStackTrace();
@@ -95,7 +160,28 @@ public class DummyContent {
     }
 
     public static void getResponse() {
-        new BackgroundTask().execute(GET_URL);
+        new BackgroundTask().execute();
+    }
+
+    public static int loopForFloor(JSONObject object, int floorLevel) {
+        int count = 0;
+        try {
+            JSONArray got = object.getJSONArray("data");
+            //int count = 0;
+            for(int i = 0; i < got.length(); i++) {
+                if (got.getJSONObject(i).getString("floor") == Integer.toString(floorLevel)) {
+                    count++;
+                }
+            }
+
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+            System.err.println("Fail to count!");
+        }
+
+        return count;
+
     }
 
     static {
